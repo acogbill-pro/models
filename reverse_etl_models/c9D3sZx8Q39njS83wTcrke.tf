0688123ca1a5bad38c6b9e69546429e84e424fa1
Segment Git Sync <friends@segment.com>
2024-09-10 12:16:28 +0000

    Reverse ETL Model 'c9D3sZx8Q39njS83wTcrke' modified by andy.cogbill@segment.com

--- reverse_etl_models/c9D3sZx8Q39njS83wTcrke.tf
+++ reverse_etl_models/c9D3sZx8Q39njS83wTcrke.tf
@@ -5,7 +5,7 @@ import {
 
 resource "segment_reverse_etl_model" "id-c9D3sZx8Q39njS83wTcrke" {
   description             = "reviews"
-  enabled                 = true
+  enabled                 = false
   name                    = "Reviews via DBT"
   query                   = "select id, anonymous_id, user_id, \ntimestamp,\nproduct_name,\nproduct_description,\nproduct_category,\ncontents as review,\nTO_NUMBER(ROUND(SNOWFLAKE.CORTEX.SENTIMENT(review)*100,0)) as sentiment_score,\n-- SNOWFLAKE.CORTEX.SUMMARIZE(review) as summary,\n-- GET(SNOWFLAKE.CORTEX.COMPLETE(\n-- 'mistral-7b',\n--      [\n--           {'role': 'system', 'content': 'You are a helpful AI assistant. Analyze the product review text and determine if the user would buy the product again. Answer with just \\\"TRUE\\\", \\\"FALSE\\\", or \\\"NULL\\\"'},\n--           {'role': 'user', 'content': review}\n--      ],\n--      {'temperature': 0.1}):choices,0):messages as will_buy_again\nfrom ACOGBILL_DEMO.SHOP_SITE.product_review;"
   query_identifier_column = "id"
