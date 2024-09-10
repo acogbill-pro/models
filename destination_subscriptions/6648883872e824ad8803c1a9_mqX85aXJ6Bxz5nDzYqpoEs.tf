fc2f436336b6d9e4cd76c6dd5f9ee0340adfa91c
Segment Git Sync <friends@segment.com>
2024-09-10 15:51:30 +0000

    Destination Subscription '6648883872e824ad8803c1a9:mqX85aXJ6Bxz5nDzYqpoEs' modified by andy.cogbill@segment.com

--- destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
+++ destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
@@ -10,5 +10,5 @@ resource "segment_destination_subscription" "id-6648883872e824ad8803c1a9_mqX85aX
   model_id       = "jVekacFFukWA38jFXYz46F"
   name           = "Review with Sentiment"
   settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"consent\":{\"@path\":\"$.context.consent\"},\"event_name\":{\"@path\":\"$.event\"},\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"@path\":\"$.properties\"},\"timestamp\":{\"@path\":\"$.receivedAt\"},\"user_id\":{\"@path\":\"$.properties.USER_ID\"}}"
-  trigger        = "event = \"new\""
+  trigger        = "event = \"new\" or event = \"updated\""
 }
