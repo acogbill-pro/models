6417b67b667ab1ef29f5180dbeb5f51e5fd29e0c
Segment Git Sync <friends@segment.com>
2024-09-10 20:30:12 +0000

    Reverse ETL Model 'jVekacFFukWA38jFXYz46F' modified by internal-service@segment.com

--- reverse_etl_models/jVekacFFukWA38jFXYz46F.tf
+++ reverse_etl_models/jVekacFFukWA38jFXYz46F.tf
@@ -7,7 +7,7 @@ resource "segment_reverse_etl_model" "id-jVekacFFukWA38jFXYz46F" {
   description             = "reviews"
   enabled                 = true
   name                    = "Reviews (DBT)"
-  query                   = "select id, anonymous_id, user_id, \ntimestamp,\nproduct_name,\nproduct_description,\nproduct_category,\nproduct_category as product_sub_category,\nPRODUCT_PRICE_USD as price,\ncontents as review,\nTO_NUMBER(ROUND(SNOWFLAKE.CORTEX.SENTIMENT(review)*100,0)) as sentiment_score\nfrom ACOGBILL_DEMO.SHOP_SITE.product_review"
+  query                   = "select id, anonymous_id, user_id, \nCONCAT(id,'-',DATE_PART(epoch_second, CURRENT_TIMESTAMP())) as generated_id,\ntimestamp,\nproduct_name,\nproduct_description,\nproduct_category,\nPRODUCT_PRICE_USD as price,\ncontents as review,\nTO_NUMBER(ROUND(SNOWFLAKE.CORTEX.SENTIMENT(review)*100,0)) as sentiment_score\nfrom ACOGBILL_DEMO.SHOP_SITE.product_review"
   query_identifier_column = "id"
   schedule_config         = "{}"
   schedule_strategy       = ""
