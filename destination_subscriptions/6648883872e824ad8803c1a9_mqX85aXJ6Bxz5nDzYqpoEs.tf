0a8ed15b67b705287e391c5a8824259f6598c832
Segment Git Sync <friends@segment.com>
2024-09-10 14:42:06 +0000

    Destination Subscription '6648883872e824ad8803c1a9:mqX85aXJ6Bxz5nDzYqpoEs' enabled by andy.cogbill@segment.com

--- destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
+++ destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs" {
   action_id      = "oiFvX4YXwyaPVysi1t7mtz"
   destination_id = "6648883872e824ad8803c1a9"
-  enabled        = false
+  enabled        = true
   model_id       = "jVekacFFukWA38jFXYz46F"
   name           = "Review with Sentiment"
   settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"consent\":{\"@path\":\"$.context.consent\"},\"event_name\":{\"@path\":\"$.event\"},\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"@path\":\"$.properties\"},\"timestamp\":{\"@path\":\"$.receivedAt\"},\"user_id\":{\"@path\":\"$.properties.USER_ID\"}}"
