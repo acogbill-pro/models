904118264d1cdcfd2cd3b53ea62bca7defd1a816
Segment Git Sync <friends@segment.com>
2024-09-10 19:01:03 +0000

    Destination Subscription '6648883872e824ad8803c1a9:mqX85aXJ6Bxz5nDzYqpoEs' modified by andy.cogbill@segment.com

--- destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
+++ destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
@@ -9,6 +9,6 @@ resource "segment_destination_subscription" "id-6648883872e824ad8803c1a9_mqX85aX
   enabled        = true
   model_id       = "jVekacFFukWA38jFXYz46F"
   name           = "Review with Sentiment"
-  settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"consent\":{\"@path\":\"$.context.consent\"},\"event_name\":{\"@path\":\"$.event\"},\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"@path\":\"$.properties\"},\"timestamp\":{\"@path\":\"$.receivedAt\"},\"user_id\":{\"@path\":\"$.properties.USER_ID\"}}"
+  settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"consent\":{\"@path\":\"$.context.consent\"},\"event_name\":{\"@path\":\"$.event\"},\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"@path\":\"$.properties\"},\"timestamp\":{\"@path\":\"$.properties.TIMESTAMP\"},\"user_id\":{\"@path\":\"$.__segment_id\"}}"
   trigger        = "event = \"new\" or event = \"updated\""
 }
