9deb3bee510dc07036bd2b362dd0ba41d98d5caf
Segment Git Sync <friends@segment.com>
2024-09-10 14:38:31 +0000

    Destination Subscription '6648883872e824ad8803c1a9:mqX85aXJ6Bxz5nDzYqpoEs' created by andy.cogbill@segment.com

---
+++ destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
@@ -0,0 +1,14 @@
+import {
+  to = segment_destination_subscription.id-6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs
+  id = "6648883872e824ad8803c1a9:mqX85aXJ6Bxz5nDzYqpoEs"
+}
+
+resource "segment_destination_subscription" "id-6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs" {
+  action_id      = "oiFvX4YXwyaPVysi1t7mtz"
+  destination_id = "6648883872e824ad8803c1a9"
+  enabled        = false
+  model_id       = "jVekacFFukWA38jFXYz46F"
+  name           = "Send Track"
+  settings       = "{}"
+  trigger        = "type = \"track\""
+}
