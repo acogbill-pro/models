6429c387d40ad74ea5e1e353b299d4a4318bfc9a
Segment Git Sync <friends@segment.com>
2024-09-11 02:47:58 +0000

    Destination Subscription '6648883872e824ad8803c1a9:mqX85aXJ6Bxz5nDzYqpoEs' modified by andy.cogbill@segment.com

--- destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
+++ destination_subscriptions/6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6648883872e824ad8803c1a9_mqX85aXJ6Bxz5nDzYqpoEs" {
   action_id      = "oiFvX4YXwyaPVysi1t7mtz"
   destination_id = "6648883872e824ad8803c1a9"
-  enabled        = true
+  enabled        = false
   model_id       = "jVekacFFukWA38jFXYz46F"
   name           = "Review with Sentiment"
   settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"consent\":{\"@path\":\"$.context.consent\"},\"event_name\":\"Product Review\",\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"ANONYMOUS_ID\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"GENERATED_ID\":{\"@path\":\"$.properties.GENERATED_ID\"},\"ID\":{\"@path\":\"$.properties.ID\"},\"PRICE\":{\"@path\":\"$.properties.PRICE\"},\"PRODUCT_CATEGORY\":{\"@path\":\"$.properties.PRODUCT_CATEGORY\"},\"PRODUCT_DESCRIPTION\":{\"@path\":\"$.properties.PRODUCT_DESCRIPTION\"},\"PRODUCT_NAME\":{\"@path\":\"$.properties.PRODUCT_NAME\"},\"REVIEW\":{\"@path\":\"$.properties.REVIEW\"},\"SENTIMENT_SCORE\":{\"@path\":\"$.properties.SENTIMENT_SCORE\"},\"TIMESTAMP\":{\"@path\":\"$.properties.TIMESTAMP\"},\"USER_ID\":{\"@path\":\"$.properties.USER_ID\"}},\"timestamp\":{\"@path\":\"$.properties.TIMESTAMP\"},\"user_id\":{\"@path\":\"$.properties.USER_ID\"}}"
