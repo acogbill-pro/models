a1107baba7527d34398800585dc317483c9170d6
Segment Git Sync <friends@segment.com>
2024-09-11 03:17:49 +0000

    Destination Subscription '6619b801712ae58c68b1bc5c:733tRbNMMoiCtc5FQedatX' modified by andy.cogbill@segment.com

--- destination_subscriptions/6619b801712ae58c68b1bc5c_733tRbNMMoiCtc5FQedatX.tf
+++ destination_subscriptions/6619b801712ae58c68b1bc5c_733tRbNMMoiCtc5FQedatX.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6619b801712ae58c68b1bc5c_733tRbNMMoiCtc5FQedatX" {
   action_id      = "oiFvX4YXwyaPVysi1t7mtz"
   destination_id = "6619b801712ae58c68b1bc5c"
-  enabled        = true
+  enabled        = false
   model_id       = "bgwPiXAwQ2GWHdAvuhev8L"
   name           = "Review with Sentiment"
   settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.ANONYMOUS_ID\"},\"consent\":{\"@path\":\"$.context.consent\"},\"enable_batching\":false,\"event_name\":\"Product Review\",\"message_id\":{\"@path\":\"$.messageId\"},\"properties\":{\"@path\":\"$.properties\"},\"timestamp\":{\"@path\":\"$.properties.TIMESTAMP\"},\"traits\":{\"@path\":\"$.properties\"},\"user_id\":{\"@path\":\"$.properties.USER_ID\"}}"
