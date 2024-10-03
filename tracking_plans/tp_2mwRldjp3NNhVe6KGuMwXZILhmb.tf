69632227e05db4ed67f1339209017b85bf516fd9
Segment Git Sync <friends@segment.com>
2024-10-03 20:05:07 +0000

    Tracking Plan 'tp_2mwRldjp3NNhVe6KGuMwXZILhmb' modified by andy.cogbill@segment.com

--- tracking_plans/tp_2mwRldjp3NNhVe6KGuMwXZILhmb.tf
+++ tracking_plans/tp_2mwRldjp3NNhVe6KGuMwXZILhmb.tf
@@ -7,6 +7,12 @@ resource "segment_tracking_plan" "id-tp_2mwRldjp3NNhVe6KGuMwXZILhmb" {
   description = null
   name        = "User Auth"
   rules = [
+    {
+      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"properties\":{\"userId\":{\"description\":\"\",\"id\":\"/properties/properties/properties/userId\",\"type\":\"string\"}},\"required\":[\"userId\"],\"type\":\"object\"},\"traits\":{\"type\":\"object\"}},\"required\":[\"properties\"],\"type\":\"object\"}"
+      key         = "User Login"
+      type        = "TRACK"
+      version     = 1
+    },
   ]
   type = "RULE_LIBRARY"
 }
