452625558d4bee9da85dd1876a525224db4d3465
Segment Git Sync <friends@segment.com>
2024-10-03 20:03:24 +0000

    Tracking Plan 'tp_2mwRldjp3NNhVe6KGuMwXZILhmb' created by andy.cogbill@segment.com

---
+++ tracking_plans/tp_2mwRldjp3NNhVe6KGuMwXZILhmb.tf
@@ -0,0 +1,12 @@
+import {
+  to = segment_tracking_plan.id-tp_2mwRldjp3NNhVe6KGuMwXZILhmb
+  id = "tp_2mwRldjp3NNhVe6KGuMwXZILhmb"
+}
+
+resource "segment_tracking_plan" "id-tp_2mwRldjp3NNhVe6KGuMwXZILhmb" {
+  description = null
+  name        = "User Auth"
+  rules = [
+  ]
+  type = "RULE_LIBRARY"
+}
