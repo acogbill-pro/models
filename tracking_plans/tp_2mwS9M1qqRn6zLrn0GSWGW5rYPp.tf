f6a37dba4b09d58f89430ad1dc6c9462d0efdff0
Segment Git Sync <friends@segment.com>
2024-10-03 20:06:54 +0000

    Tracking Plan 'tp_2mwS9M1qqRn6zLrn0GSWGW5rYPp' modified by andy.cogbill@segment.com

--- tracking_plans/tp_2mwS9M1qqRn6zLrn0GSWGW5rYPp.tf
+++ tracking_plans/tp_2mwS9M1qqRn6zLrn0GSWGW5rYPp.tf
@@ -8,7 +8,7 @@ resource "segment_tracking_plan" "id-tp_2mwS9M1qqRn6zLrn0GSWGW5rYPp" {
   name        = "User Profile"
   rules = [
     {
-      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"type\":\"object\"},\"traits\":{}},\"type\":\"object\"}"
+      json_schema = "{\"$schema\":\"http://json-schema.org/draft-07/schema#\",\"properties\":{\"context\":{},\"properties\":{\"properties\":{\"userId\":{\"description\":\"\",\"id\":\"/properties/properties/properties/userId\",\"type\":\"string\"}},\"required\":[\"userId\"],\"type\":\"object\"},\"traits\":{}},\"required\":[\"properties\"],\"type\":\"object\"}"
       key         = "Property Library Rule"
       type        = "TRACK"
       version     = 1
