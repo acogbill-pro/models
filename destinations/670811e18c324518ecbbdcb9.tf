cc6a92fe05bbff0ebc64f3b24331c2e218a29945
Segment Git Sync <friends@segment.com>
2024-10-10 17:41:54 +0000

    Destination '670811e18c324518ecbbdcb9' created by andy.cogbill@segment.com

---
+++ destinations/670811e18c324518ecbbdcb9.tf
@@ -0,0 +1,18 @@
+import {
+  to = segment_destination.id-670811e18c324518ecbbdcb9
+  id = "670811e18c324518ecbbdcb9"
+}
+
+resource "segment_destination" "id-670811e18c324518ecbbdcb9" {
+  enabled = false
+  metadata = {
+    contacts          = null
+    id                = "5850d8b680412f644ff55df2"
+    partner_owned     = false
+    region_endpoints  = ["US", "EU"]
+    supported_regions = null
+  }
+  name      = "Repeater"
+  settings  = "{\"endpoint\":\"https://api.segment.io\",\"repeatKeys\":[]}"
+  source_id = "2TapZFYdqrQMgoXiU2JZCU"
+}
