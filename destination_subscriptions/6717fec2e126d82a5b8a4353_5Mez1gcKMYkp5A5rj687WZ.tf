999c35c14913e51e8cb7123a6f690485d3d00c71
Segment Git Sync <friends@segment.com>
2024-10-22 19:46:15 +0000

    Destination Subscription '6717fec2e126d82a5b8a4353:5Mez1gcKMYkp5A5rj687WZ' modified by andy.cogbill@segment.com

--- destination_subscriptions/6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ.tf
+++ destination_subscriptions/6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ.tf
@@ -9,6 +9,6 @@ resource "segment_destination_subscription" "id-6717fec2e126d82a5b8a4353_5Mez1gc
   enabled        = false
   model_id       = null
   name           = "Send Identify"
-  settings       = "{}"
+  settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.anonymous_id_c\"},\"consent\":{\"@path\":\"$.context.consent\"},\"engage_space\":\"x2zPLJyKwsPESrDJLQiAGAlIWYH5IYlk\",\"message_id\":{\"@path\":\"$.messageId\"},\"timestamp\":{\"@path\":\"$.receivedAt\"},\"traits\":{\"@path\":\"$.properties\"},\"user_id\":{\"@path\":\"$.properties.user_id_c\"}}"
   trigger        = "event=\"object-destination-data\""
 }
