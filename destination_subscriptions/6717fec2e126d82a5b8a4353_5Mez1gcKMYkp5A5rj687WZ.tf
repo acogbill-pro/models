cb0ee5da94626b8c1b9bc2cd4f1514c04f53e796
Segment Git Sync <friends@segment.com>
2024-10-22 19:36:54 +0000

    Destination Subscription '6717fec2e126d82a5b8a4353:5Mez1gcKMYkp5A5rj687WZ' created by andy.cogbill@segment.com

---
+++ destination_subscriptions/6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ.tf
@@ -0,0 +1,14 @@
+import {
+  to = segment_destination_subscription.id-6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ
+  id = "6717fec2e126d82a5b8a4353:5Mez1gcKMYkp5A5rj687WZ"
+}
+
+resource "segment_destination_subscription" "id-6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ" {
+  action_id      = "vm2tNA5U1zxUusQkdC4D7Z"
+  destination_id = "6717fec2e126d82a5b8a4353"
+  enabled        = false
+  model_id       = null
+  name           = "Send Identify"
+  settings       = "{}"
+  trigger        = "event=\"object-destination-data\""
+}
