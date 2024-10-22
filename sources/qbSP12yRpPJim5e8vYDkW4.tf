ddc702c5d4cbac0e096b66b465ad7859e69143a7
Segment Git Sync <friends@segment.com>
2024-10-22 19:13:23 +0000

    Source 'qbSP12yRpPJim5e8vYDkW4' modified by andy.cogbill@segment.com

--- sources/qbSP12yRpPJim5e8vYDkW4.tf
+++ sources/qbSP12yRpPJim5e8vYDkW4.tf
@@ -4,12 +4,12 @@ import {
 }
 
 resource "segment_source" "id-qbSP12yRpPJim5e8vYDkW4" {
-  enabled = false
+  enabled = true
   labels  = null
   metadata = {
     id = "2baks93n"
   }
   name     = "Salesforce Profiles"
-  settings = "{\"createdAt\":\"\",\"createdBy\":\"\",\"displayName\":\"\",\"instance-url\":\"\",\"salesforce-environment\":\"production\"}"
+  settings = "{\"createdAt\":\"2024-10-22T19:13:08.886Z\",\"createdBy\":\"andy.cogbill@segment.com\",\"displayName\":\"andy.cogbill@segment.com\",\"instance-url\":\"https://segment.my.salesforce.com\",\"salesforce-environment\":\"production\"}"
   slug     = "salesforce_profiles"
 }
