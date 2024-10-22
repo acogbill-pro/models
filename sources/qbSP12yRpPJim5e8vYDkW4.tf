6e42fbef8d551cb095b48167a6f10e99e8d1379c
Segment Git Sync <friends@segment.com>
2024-10-22 19:12:46 +0000

    Source 'qbSP12yRpPJim5e8vYDkW4' modified by andy.cogbill@segment.com

---
+++ sources/qbSP12yRpPJim5e8vYDkW4.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-qbSP12yRpPJim5e8vYDkW4
+  id = "qbSP12yRpPJim5e8vYDkW4"
+}
+
+resource "segment_source" "id-qbSP12yRpPJim5e8vYDkW4" {
+  enabled = false
+  labels  = null
+  metadata = {
+    id = "2baks93n"
+  }
+  name     = "Salesforce Profiles"
+  settings = "{\"createdAt\":\"\",\"createdBy\":\"\",\"displayName\":\"\",\"instance-url\":\"\",\"salesforce-environment\":\"production\"}"
+  slug     = "salesforce_profiles"
+}
