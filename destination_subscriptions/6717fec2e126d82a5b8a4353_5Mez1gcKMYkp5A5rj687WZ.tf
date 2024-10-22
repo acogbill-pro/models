5601e773c58a8bc371bb59534854a775f515c197
Segment Git Sync <friends@segment.com>
2024-10-22 19:46:17 +0000

    Destination Subscription '6717fec2e126d82a5b8a4353:5Mez1gcKMYkp5A5rj687WZ' enabled by andy.cogbill@segment.com

--- destination_subscriptions/6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ.tf
+++ destination_subscriptions/6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ.tf
@@ -6,7 +6,7 @@ import {
 resource "segment_destination_subscription" "id-6717fec2e126d82a5b8a4353_5Mez1gcKMYkp5A5rj687WZ" {
   action_id      = "vm2tNA5U1zxUusQkdC4D7Z"
   destination_id = "6717fec2e126d82a5b8a4353"
-  enabled        = false
+  enabled        = true
   model_id       = null
   name           = "Send Identify"
   settings       = "{\"anonymous_id\":{\"@path\":\"$.properties.anonymous_id_c\"},\"consent\":{\"@path\":\"$.context.consent\"},\"engage_space\":\"x2zPLJyKwsPESrDJLQiAGAlIWYH5IYlk\",\"message_id\":{\"@path\":\"$.messageId\"},\"timestamp\":{\"@path\":\"$.receivedAt\"},\"traits\":{\"@path\":\"$.properties\"},\"user_id\":{\"@path\":\"$.properties.user_id_c\"}}"
