397a46ed9fc6286c92eb69319a5278f2502824bf
Segment Git Sync <friends@segment.com>
2025-04-17 09:59:53 +0000

    Reverse ETL Model 'x59AiyJ3ncuSLHdoRuSPuc' created by andy.cogbill@segment.com

---
+++ reverse_etl_models/x59AiyJ3ncuSLHdoRuSPuc.tf
@@ -0,0 +1,13 @@
+import {
+  to = segment_reverse_etl_model.id-x59AiyJ3ncuSLHdoRuSPuc
+  id = "x59AiyJ3ncuSLHdoRuSPuc"
+}
+
+resource "segment_reverse_etl_model" "id-x59AiyJ3ncuSLHdoRuSPuc" {
+  description             = "Programatically generated rETL model that is associated with an Engage rETL audience"
+  enabled                 = true
+  name                    = "Linked Audience rETL Model"
+  query                   = "SELECT 1"
+  query_identifier_column = "matchId"
+  source_id               = "wLoTFxk9DuredsGx6ECmgJ"
+}
