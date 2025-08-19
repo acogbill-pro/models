7dff7d2adbb09e0a0031485fee238598e452fce8
Segment Git Sync <friends@segment.com>
2025-08-19 17:06:01 +0000

    Full sync

---
+++ user_groups/2ZPUxE8spb0LVUuH54cyj3Om4S8.tf
@@ -0,0 +1,77 @@
+import {
+  to = segment_user_group.id-2ZPUxE8spb0LVUuH54cyj3Om4S8
+  id = "2ZPUxE8spb0LVUuH54cyj3Om4S8"
+}
+
+resource "segment_user_group" "id-2ZPUxE8spb0LVUuH54cyj3Om4S8" {
+  members = ["eric.walters@segment.com", "kurquhart@twilio.com", "neil.yashinsky@segment.com", "scott.sales@segment.com"]
+  name    = "Teammates"
+  permissions = [
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DFJmy6Vln5DZyS3SGBXjTdN"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DGbc2C28CSKzqm4RCJlYAvx"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DKQ95UMoCKw1y9syxModCaC"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1Mc8DLRqfeBpxN5cNBjK0n4oTPX"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "1ZxNOhjYFrqvYCPreLHBNTnepB4"
+    },
+    {
+      resources = [
+        {
+          id = "cWBgjiLvqmfWSiUjyonfVR"
+          labels = [
+          ]
+          type = "WORKSPACE"
+        },
+      ]
+      role_id = "2Neg5AUm9pCOWXGQYzKOiBqpvLk"
+    },
+  ]
+}
